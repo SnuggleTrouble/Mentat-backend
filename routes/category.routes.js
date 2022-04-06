a2acaff9c4ad3dd40df9b155773cf952771966c7
SnuggleTrouble <stabjo@outlook.com>
2022-04-06 13:23:36 +0200

    Add code to the category route

---
+++ routes/category.routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const Category = require("../models/Category.model");
+
+const router = express.Router();
+
+// Create a category
+router.post("/create", async (req, res) => {
+  const { name } = req.body;
+  const category = await Category.create({
+    name,
+    user: req.jwtPayload.user._id,
+  });
+  res.status(200).json(category);
+});
+
+module.exports = router;
