645c4467b06743c87aa9963fbbffcf0162b83a53
SnuggleTrouble <stabjo@outlook.com>
2022-04-12 21:30:18 +0200

    added username to the user model

--- models/User.model.js
+++ models/User.model.js
@@ -11,6 +11,10 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  userName: {
+    type: String,
+    required: true,
+  },
   email: {
     type: String,
     required: true,
