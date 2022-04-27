4dd2b76df88a5aaf4bc4dc4d8f96852cb0ff4c6c
SnuggleTrouble <stabjo@outlook.com>
2022-04-27 22:21:41 +0200

    Fixes to the comment feature to make it easier to read

--- models/Comment.model.js
+++ models/Comment.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const commentSchema = new Schema({
-  content: {
+  commentContent: {
     type: String,
     required: true,
   },
